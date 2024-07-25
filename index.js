6f481736083b0e616575d8d0e6f9401ed5c4f060
romilamufc <romila@triconinfotech.com>
2024-07-25 17:36:16 +0530

    cors policy issue resolved

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./app');
 const port = process.env.PORT || 3000;
-
+const cors = require('cors');
+app.use(cors());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
